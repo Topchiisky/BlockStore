d25f8981c07d208bc300dda7ebb447849bc1a408
Topchiisky <ivan.topchiisky@gmail.com>
2018-03-10 15:02:12 +0200

    some more logic and stuff, and a backup before ipfs

---
+++ ui/js/contract.js
@@ -0,0 +1,269 @@
+var ContractABI = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "checkCurrentDonations",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_itemIdx",
+                "type": "uint256"
+            },
+            {
+                "name": "_buyerInfo",
+                "type": "string"
+            }
+        ],
+        "name": "buyItem",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_itemIdx",
+                "type": "uint256"
+            }
+        ],
+        "name": "confirmItemReceived",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "idx",
+                "type": "uint256"
+            }
+        ],
+        "name": "viewItemForSale",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            },
+            {
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_itemIdx",
+                "type": "uint256"
+            }
+        ],
+        "name": "cancelSale",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_itemDescription",
+                "type": "string"
+            },
+            {
+                "name": "_price",
+                "type": "uint256"
+            }
+        ],
+        "name": "registerItemForSale",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "checkItemsForSaleLength",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "checkBalance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "withdrawDonations",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "checkTotalDonations",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "donate",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "_itemDescription",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "name": "_price",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "_time",
+                "type": "uint256"
+            }
+        ],
+        "name": "registerItem",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "_idx",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "_itemDescription",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "name": "_time",
+                "type": "uint256"
+            }
+        ],
+        "name": "itemBought",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "_itemIdx",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "itemDescription",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "name": "_time",
+                "type": "uint256"
+            }
+        ],
+        "name": "confirmItem",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [],
+        "name": "donationMade",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "_itemIdx",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "itemDescription",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "name": "_time",
+                "type": "uint256"
+            }
+        ],
+        "name": "itemCanceled",
+        "type": "event"
+    }
+];
+
+var ContractAddress = "0x36275d539511f77abc145d905c74ffaecabdf416";
--- ui/js/homepage.js
+++ ui/js/homepage.js
@@ -3,31 +3,28 @@
 var Shuffle = window.Shuffle;
 
 var Demo = function (element) {
-  this.element = element;
+    this.element = element;
 
-  this.shuffle = new Shuffle(element, {
-    itemSelector: '.picture-item',
-    sizer: element.querySelector('.my-sizer-element'),
-  });
+    this.shuffle = new Shuffle(element, {
+        itemSelector: '.picture-item',
+    });
 
-  // Log events.
-  this.addShuffleEventListeners();
+    // Log events.
+    this.addShuffleEventListeners();
 
-  this._activeFilters = [];
+    this._activeFilters = [];
 
-  this.addFilterButtons();
-  this.addSorting();
-  this.addSearchFilter();
-
-  this.mode = 'exclusive';
+    this.addFilterButtons();
+    this.addSorting();
+    this.addSearchFilter();
 };
 
 Demo.prototype.toggleMode = function () {
-  if (this.mode === 'additive') {
-    this.mode = 'exclusive';
-  } else {
-    this.mode = 'additive';
-  }
+    if (this.mode === 'additive') {
+        this.mode = 'exclusive';
+    } else {
+        this.mode = 'additive';
+    }
 };
 
 /**
@@ -35,132 +32,118 @@ Demo.prototype.toggleMode = function () {
  * for them like you normally would (with jQuery for example).
  */
 Demo.prototype.addShuffleEventListeners = function () {
-  this.shuffle.on(Shuffle.EventType.LAYOUT, function (data) {
-    console.log('layout. data:', data);
-  });
+    this.shuffle.on(Shuffle.EventType.LAYOUT, function (data) {
+        console.log('layout. data:', data);
+    });
 
-  this.shuffle.on(Shuffle.EventType.REMOVED, function (data) {
-    console.log('removed. data:', data);
-  });
+    this.shuffle.on(Shuffle.EventType.REMOVED, function (data) {
+        console.log('removed. data:', data);
+    });
 };
 
 Demo.prototype.addFilterButtons = function () {
-  var options = document.querySelector('.filter-options');
+    var options = document.querySelector('.filter-options');
 
-  if (!options) {
-    return;
-  }
+    if (!options) {
+        return;
+    }
 
-  var filterButtons = Array.from(options.children);
+    var filterButtons = Array.from(options.children);
 
-  filterButtons.forEach(function (button) {
-    button.addEventListener('click', this._handleFilterClick.bind(this), false);
-  }, this);
+    filterButtons.forEach(function (button) {
+        button.addEventListener('click', this._handleFilterClick.bind(this), false);
+    }, this);
 };
 
 Demo.prototype._handleFilterClick = function (evt) {
-  var btn = evt.currentTarget;
-  var isActive = btn.classList.contains('active');
-  var btnGroup = btn.getAttribute('data-group');
+    var btn = evt.currentTarget;
+    var isActive = btn.classList.contains('active');
+    var btnGroup = btn.getAttribute('data-group');
 
-  // You don't need _both_ of these modes. This is only for the demo.
-
-  // For this custom 'additive' mode in the demo, clicking on filter buttons
-  // doesn't remove any other filters.
-  if (this.mode === 'additive') {
-    // If this button is already active, remove it from the list of filters.
-    if (isActive) {
-      this._activeFilters.splice(this._activeFilters.indexOf(btnGroup));
-    } else {
-      this._activeFilters.push(btnGroup);
-    }
-
-    btn.classList.toggle('active');
-
-    // Filter elements
-    this.shuffle.filter(this._activeFilters);
-
-  // 'exclusive' mode lets only one filter button be active at a time.
-  } else {
-    this._removeActiveClassFromChildren(btn.parentNode);
+    this._removeActiveClassFromChildren(btn.parentNode, btn);
 
     var filterGroup;
     if (isActive) {
-      //btn.classList.remove('active');
-      filterGroup = Shuffle.ALL_ITEMS;
+        //btn.classList.remove('active');
+        //btn.setAttribute("aria-pressed", false);
+        filterGroup = Shuffle.ALL_ITEMS;
     } else {
-      //btn.classList.add('active');
-      filterGroup = btnGroup;
+        //btn.classList.add('active');
+        //btn.setAttribute("aria-pressed", true);
+        filterGroup = btnGroup;
     }
 
     this.shuffle.filter(filterGroup);
-  }
 };
 
-Demo.prototype._removeActiveClassFromChildren = function (parent) {
-  var children = parent.children;
-  for (var i = children.length - 1; i >= 0; i--) {
-    children[i].classList.remove('active');
-  }
+Demo.prototype._removeActiveClassFromChildren = function (parent, excludeBtn) {
+    var children = parent.children;
+    for (var i = children.length - 1; i >= 0; i--) {
+        if (children[i] == excludeBtn) {
+            continue;
+        }
+        children[i].classList.remove('active');
+        children[i].setAttribute("aria-pressed", false);
+    }
 };
 
 Demo.prototype.addSorting = function () {
-  var buttonGroup = document.querySelector('.sort-options');
+    var buttonGroup = document.querySelector('.sort-options');
 
-  if (!buttonGroup) {
-    return;
-  }
+    if (!buttonGroup) {
+        return;
+    }
 
-  buttonGroup.addEventListener('change', this._handleSortChange.bind(this));
+    buttonGroup.addEventListener('change', this._handleSortChange.bind(this));
 };
 
 Demo.prototype._handleSortChange = function (evt) {
-  // Add and remove `active` class from buttons.
-  var wrapper = evt.currentTarget;
-  var buttons = Array.from(evt.currentTarget.children);
-  buttons.forEach(function (button) {
-    if (button.querySelector('input').value === evt.target.value) {
-      button.classList.add('active');
-    } else {
-      button.classList.remove('active');
+    // Add and remove `active` class from buttons.
+    var wrapper = evt.currentTarget;
+    var buttons = Array.from(evt.currentTarget.children);
+    buttons.forEach(function (button) {
+        if (button.querySelector('input').value === evt.target.value) {
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
+
+    // Create the sort options to give to Shuffle.
+    var value = evt.target.value;
+    var options = {};
+
+    function sortByDate(element) {
+        return element.getAttribute('data-created');
     }
-  });
 
-  // Create the sort options to give to Shuffle.
-  var value = evt.target.value;
-  var options = {};
+    function sortByTitle(element) {
+        return element.getAttribute('data-title').toLowerCase();
+    }
 
-  function sortByDate(element) {
-    return element.getAttribute('data-created');
-  }
+    if (value === 'date-created') {
+        options = {
+            reverse: true,
+            by: sortByDate,
+        };
+    } else if (value === 'title') {
+        options = {
+            by: sortByTitle,
+        };
+    }
 
-  function sortByTitle(element) {
-    return element.getAttribute('data-title').toLowerCase();
-  }
-
-  if (value === 'date-created') {
-    options = {
-      reverse: true,
-      by: sortByDate,
-    };
-  } else if (value === 'title') {
-    options = {
-      by: sortByTitle,
-    };
-  }
-
-  this.shuffle.sort(options);
+    this.shuffle.sort(options);
 };
 
 // Advanced filtering
 Demo.prototype.addSearchFilter = function () {
-  var searchInput = document.querySelector('.js-shuffle-search');
+    var searchInput = document.querySelector('.js-shuffle-search');
 
-  if (!searchInput) {
-    return;
-  }
+    if (!searchInput) {
+        return;
+    }
 
-  searchInput.addEventListener('keyup', this._handleSearchKeyup.bind(this));
+    searchInput.addEventListener('keyup', this._handleSearchKeyup.bind(this));
 };
 
 /**
@@ -168,29 +151,132 @@ Demo.prototype.addSearchFilter = function () {
  * @param {Event} evt Event object.
  */
 Demo.prototype._handleSearchKeyup = function (evt) {
-  var searchText = evt.target.value.toLowerCase();
+    var searchText = evt.target.value.toLowerCase();
 
-  this.shuffle.filter(function (element, shuffle) {
+    this.shuffle.filter(function (element, shuffle) {
 
-    // If there is a current filter applied, ignore elements that don't match it.
-    if (shuffle.group !== Shuffle.ALL_ITEMS) {
-      // Get the item's groups.
-      var groups = JSON.parse(element.getAttribute('data-groups'));
-      var isElementInCurrentGroup = groups.indexOf(shuffle.group) !== -1;
+        // If there is a current filter applied, ignore elements that don't match it.
+        if (shuffle.group !== Shuffle.ALL_ITEMS) {
+            // Get the item's groups.
+            var groups = JSON.parse(element.getAttribute('data-groups'));
+            var isElementInCurrentGroup = groups.indexOf(shuffle.group) !== -1;
 
-      // Only search elements in the current group
-      if (!isElementInCurrentGroup) {
-        return false;
-      }
+            // Only search elements in the current group
+            if (!isElementInCurrentGroup) {
+                return false;
+            }
+        }
+
+        var titleElement = element.querySelector('.picture-item__title');
+        var titleText = titleElement.textContent.toLowerCase().trim();
+
+        return titleText.indexOf(searchText) !== -1;
+    });
+};
+
+var MyWeb3 = function () {
+    if (!this.checkForWeb3()) {
+        return;
     }
 
-    var titleElement = element.querySelector('.picture-item__title');
-    var titleText = titleElement.textContent.toLowerCase().trim();
+    this.setUpDefaultAccount();
+    this.createContract();
+    this.getTotalDonations();
+    this.registerDonationEvent();
+    console.log('we have web3 provider GO GO GO');
+};
 
-    return titleText.indexOf(searchText) !== -1;
-  });
+MyWeb3.prototype.checkForWeb3 = function () {
+    try {
+        if (typeof window.web3 !== 'undefined') {
+            window.web3 = new Web3(web3.currentProvider);
+        } else {
+            window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        }
+    } catch (err) {
+        this.showNoWeb3Error();
+        return false;
+    }
+
+    return true;
+};
+
+MyWeb3.prototype.showNoWeb3Error = function () {
+    alert("please Install metamask to use this app");
+};
+
+MyWeb3.prototype.setUpDefaultAccount = function () {
+    window.web3.eth.defaultAccount = window.web3.eth.accounts[0];
+};
+
+MyWeb3.prototype.createContract = function () {
+    MyWeb3.contractABI = window.web3.eth.contract(ContractABI);
+    MyWeb3.contract = MyWeb3.contractABI.at(ContractAddress);
+};
+
+MyWeb3.prototype.getTotalDonations = function () {
+    MyWeb3.contract.checkTotalDonations((err, res) => {
+        if (!err) {
+            var valToSet = $('#donationRaised').text();
+            valToSet = valToSet.split(":")[0] + ": " + window.web3.fromWei(res.toString(), 'ether') + " ETH";
+            $('#donationRaised').text(valToSet);
+        }
+    });
+};
+
+MyWeb3.prototype.makeDonation = function () {
+    var modalDonationForm = $('#modalDonationForm');
+    var donationInput = $('#modalDonationInput');
+    var donation = donationInput.val();
+    if (!$.isNumeric(donation)) {
+        return;
+    }
+    MyWeb3.contract.donate({ from: window.web3.eth.defaultAccount, gas: 3000000, value: window.web3.toWei(donation, 'ether') }, (err, res) => {
+        if (!err) {
+            //TODO add link to monitoring modal
+            console.log(res);
+        }
+    });
+
+    modalDonationForm.find('.close').click()
+};
+
+MyWeb3.prototype.registerDonationEvent = function () {
+    MyWeb3.donationEvent = MyWeb3.contract.donationMade({}, 'latest');
+    MyWeb3.donationEvent.watch(function (err, result) {
+        if (!err) {
+            window.myWeb3.getTotalDonations();
+        }
+    });
+};
+
+var CustomHandlers = function () {
+};
+
+CustomHandlers.prototype.addNumericValidation = function (el) {
+    el.on('keyup', function () {
+        if ($.isNumeric(el.val())) {
+            el.removeClass('invalid').addClass('valid');
+        } else {
+            el.removeClass('valid').addClass('invalid');
+        }
+
+        if (el.val().length == 0) {
+            el.removeClass('valid').removeClass('invalid');
+        }
+    });
+};
+
+CustomHandlers.prototype.addMakeDonation = function (el) {
+    el.on('click', function () {
+        window.myWeb3.makeDonation();
+    });
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  window.demo = new Demo(document.getElementById('grid'));
+    window.demo = new Demo(document.getElementById('grid'));
+    window.myWeb3 = new MyWeb3();
+    window.customHandlers = new CustomHandlers();
+    window.customHandlers.addNumericValidation($('#modalDonationInput'));
+    window.customHandlers.addMakeDonation($('#sendDonation'));
 });
